dd9ffc5672a65cb79a56a32c540a942453930a63
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-02-26 21:33:49 +0100

    Create paint class

---
+++ paint.js
@@ -0,0 +1,9 @@
+class paint {
+    constructor(){
+        this.color = '#000000';
+        this.tool = 'pen'; // circle, rect, line
+        this.lineWidth = 1;        
+    }
+}
+
+var p = new paint();
