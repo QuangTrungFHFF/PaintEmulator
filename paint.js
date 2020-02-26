7e926019d3282b91c04a730c68bc73834448e003
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-02-26 22:13:18 +0100

    Find mouse position

--- paint.js
+++ paint.js
@@ -1,8 +1,35 @@
 class paint {
     constructor(){
+        this.canvas = document.getElementById('board');
+        this.context = this.canvas.getContext('2d');
         this.color = '#000000';
         this.tool = 'pen'; // circle, rect, line
-        this.lineWidth = 1;        
+        this.lineWidth = 1;  
+        
+        //listen mouse event
+        this.listenEvent();
+    }
+
+    getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
+    
+    mousedown() {
+        
+    }
+
+
+    listenEvent() {
+        this.canvas.addEventListener('mousedown', this.mousedown());
+        this.canvas.addEventListener('mousedown', this.mousedown());
+    }
+
+    drawLine(startX, startY, endX, endY){
+
     }
 }
 
