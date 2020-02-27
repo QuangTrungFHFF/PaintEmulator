84494d0e867f99f78cf86dbddc994c6641645b77
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-02-27 14:03:59 +0100

    Add mouse listener

--- paint.js
+++ paint.js
@@ -1,13 +1,24 @@
+"use strict"
 class paint {
     constructor(){
         this.canvas = document.getElementById('board');
+        this.canvas.width = 1200;
+        this.canvas.height = 800;
         this.context = this.canvas.getContext('2d');
-        this.color = '#000000';
+        this.color = '#ff0000';
         this.tool = 'pen'; // circle, rect, line
-        this.lineWidth = 1;  
+        this.lineWidth = 5;  
+
+        this.currentPos = {
+            x: 0,
+            y: 0
+        }
+        this.drawing = false;
         
         //listen mouse event
         this.listenEvent();
+
+        this.drawLine(10, 10, 100, 100);
     }
 
     getMousePos(canvas, evt) {
@@ -18,17 +29,40 @@ class paint {
         };
     }
     
-    mousedown() {
-        
+    mousedown(event) {
+        let mousePos = this.getMousePos(event);
+        console.log("click");
+    }
+
+    mousemove(event) {
+        console.log("click");
+    }
+
+    mouseup(event) {
+        console.log("click");
     }
 
 
     listenEvent() {
-        this.canvas.addEventListener('mousedown', this.mousedown());
-        this.canvas.addEventListener('mousedown', this.mousedown());
+        this.canvas.addEventListener(
+            'mousedown', 
+            (event) => this.mousedown(event)); 
+        this.canvas.addEventListener(
+            'mousemove', 
+            (event) => this.mousemove(event));   
+        this.canvas.addEventListener(
+            'mouseup', 
+            (event) => this.mouseup(event));         
     }
 
     drawLine(startX, startY, endX, endY){
+        this.context.lineWidth = this.lineWidth;
+        this.context.strokeStyle = this.color;
+        this.context.beginPath();
+        this.context.moveTo(startX, startY);
+        this.context.lineTo(endX, endY);
+        this.context.stroke();
+
 
     }
 }
